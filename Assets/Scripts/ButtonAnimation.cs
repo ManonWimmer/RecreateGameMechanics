5806ded6df3e78aa92a8a0f5f945c65a81a416f8
Manon Wimmer <manonwimmer30@gmail.com>
2024-02-05 22:51:59 +0100

    LIS - new photoshop buttons XBOX (A, X, B, Y) x3 each + animations on scene

--- Assets/Scripts/ButtonAnimation.cs
+++ Assets/Scripts/ButtonAnimation.cs
@@ -8,14 +8,7 @@ public class ButtonAnimation : MonoBehaviour
     // ----- FIELDS ----- //
     [SerializeField] Sprite _sprite1;
     [SerializeField] Sprite _sprite2;
-    //[SerializeField] Sprite _sprite3;
-
-    private float _textOffsetX;
-    private float _textOffsetY;
-
-    private float _textOffsetTime1;
-    private float _textOffsetTime2;
-    private float _textOffsetTime3;
+    [SerializeField] Sprite _sprite3;
 
     private Coroutine _coroutine;
     private Image _image;
@@ -23,10 +16,6 @@ public class ButtonAnimation : MonoBehaviour
 
     private void Start()
     {
-        _textOffsetTime1 = UIManager.instance.TextOffsetTime1;
-        _textOffsetTime2 = UIManager.instance.TextOffsetTime2;
-        _textOffsetTime3 = UIManager.instance.TextOffsetTime3;
-
         _image = transform.GetComponent<Image>();
     }
 
@@ -45,14 +34,14 @@ public class ButtonAnimation : MonoBehaviour
 
     private IEnumerator TextOffset()
     {
-        yield return new WaitForSeconds(_textOffsetTime1);
+        yield return new WaitForSeconds(0.35f);
        _image.sprite = _sprite1;
 
-        yield return new WaitForSeconds(_textOffsetTime2);
+        yield return new WaitForSeconds(0.35f);
         _image.sprite = _sprite2;
 
-        yield return new WaitForSeconds(_textOffsetTime3);
-        //_image.sprite = _sprite3;
+        yield return new WaitForSeconds(0.35f);
+        _image.sprite = _sprite3;
 
         yield return StartCoroutine(TextOffset());
     }
